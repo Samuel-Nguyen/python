64df6307b70a64a79e3b3a4967269b2c221f4e09
Samuel-Nguyen <samueluit@gmail.com>
2018-03-17 16:58:25 +0700

    Update pylint

--- tinh_S.py
+++ tinh_S.py
@@ -1,9 +1,9 @@
-#S=(x*x+1)**n
+# S=(x*x+1)**n
 
-n=int(input("n= "))
-x=int(input("x= "))
-s=(x*x+1)
-kq=1
-for i in range (n):
-    kq*=s
-print (kq)
+n = int(input("n= "))
+x = int(input("x= "))
+s = (x*x+1)
+kq = 1
+for i in range(n):
+    kq *= s
+print(kq)
