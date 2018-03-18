fcb304473dfeef6869eae5d2a27729c4b5886fcc
Samuel-Nguyen <samueluit@gmail.com>
2018-03-19 02:44:30 +0700

    Update smell code

--- tinh_A.py
+++ tinh_A.py
@@ -1,12 +1,12 @@
-#A=(x**2+x+1)**n + (x**2-x+1)**n
-#n=3 x=2
-n=int(input("nhap n: "))
-x=eval(input("nhap x: "))
-a1=(x*x+x+1)
-a2=(x*x-x+1)
-a=1
+# A=(x**2+x+1)**n + (x**2-x+1)**n
+# n=3 x=2
+n = int(input("nhap n: "))
+x = eval(input("nhap x: "))
+a1 = (x*x+x+1)
+a2 = (x*x-x+1)
+a = 1
 for i in range(n):
-    a*=a1
+    a *= a1
 for o in range(n):
-    a*=a2
-print (a1,a2,a)
+    a *= a2
+print(a1, a2, a)
--- vidu_while.py
+++ vidu_while.py
@@ -1,8 +1,8 @@
-so=int(input("nhap 1 so nguyen duong: "))
-i=1
-tong=0
-while (i<=so):
-    if i%2==0:
-        tong+=i
-    i+=1
-print (tong)
+so = int(input("nhap 1 so nguyen duong: "))
+i = 1
+tong = 0
+while (i <= so):
+    if i % 2 == 0:
+        tong += i
+    i += 1
+print(tong)
