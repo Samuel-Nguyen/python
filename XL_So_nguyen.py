9d251b497ee5b5ce7e2806c09c604f22f3bf9a21
Samuel-Nguyen <samueluit@gmail.com>
2018-04-07 21:36:47 +0700

    Update

--- XL_So_nguyen.py
+++ XL_So_nguyen.py
@@ -1,19 +1,27 @@
 def NhapDanhSachSoNguyen(danhSach):
     tt=1
     print('Nhap vao danh sach cac so nguyen, ket thuc -1')
+    chuoi=''
     while tt!=-1:
-        tt=int(input(' '))
+        tt=int(input(''))
         if tt!=-1:
+            print(type(tt))
             danhSach.append(tt)
+            print(danhSach)
+            chuoi += '{} '.format(tt)
+    ghiFile(1, chuoi)
 def InDanhSachSoNguyen(danhSach):
     chuoi=''
     for so in danhSach:
         chuoi += str(so)+' '
     print(chuoi)
+    ghiFile(2, chuoi)
 def TongDanhSach(danhSach):
     tong=0
     for so in danhSach:
         tong+=so
+    print(tong)
+    ghiFile(3, '{} '.format(tong))
 def InDanhSole(danhSach):
     chuoi=''
     for so in danhSach:
@@ -42,5 +50,10 @@ def inDanhSachSoNguyenTo(danhSach):
         if kiemTraSoNguyenTo(so):
             chuoi += str(so) + ' '
     print(chuoi)
+    ghiFile(4, chuoi)
     
-    
+def ghiFile(causo, fileGhi):
+    with open("DSThi.txt", "a") as text_file:
+        text_file.write('Cau so ' + str(causo) + ': ')
+        text_file.write(str.format(fileGhi))
+        text_file.write('\n')
--- baitapSoNguyen.py
+++ baitapSoNguyen.py
@@ -3,3 +3,4 @@ danhSach=[]
 XL_So_nguyen.NhapDanhSachSoNguyen(danhSach)
 XL_So_nguyen.InDanhSachSoNguyen(danhSach)
 XL_So_nguyen.inDanhSachSoNguyenTo(danhSach)
+XL_So_nguyen.TongDanhSach(danhSach)
